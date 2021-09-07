892f00a1c7b0b9e4e45e31cb369403fc6792663c
Humayan Al Rosid <89333804+humayanar@users.noreply.github.com>
2021-09-07 23:39:18 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,57 +1,48 @@
-# simple calculator 
+# Simple Calculator by Sony [First Project]
 
-# instructions head line
-print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-print("-------FOLLOW THE INSTRUCTIONS--------")
-print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+print("\n\tPYTHON SIMPLE CALCULATOR")
+instraction = "Read the rules carefully:\n 1. A is for Addition.\n 2. B is for Subtraction.\n 3. C is for Multiplication.\n 4. D is for Division."
+print(instraction)
 
-# rules of calculation
-instructions = "A is for Addition,\n B is for Subtraction, \n C is for Multiplication, \n D is for Division,"
-print(instructions)
+# Addition function
+def Addition(x, y):
+    return x + y
+# Subtraction function
+def Subtraction(x, y):
+    return x - y
+#Multiplication function
+def Multiplication(x, y):
+    return x * y
+#Division function
+def Division(x, y):
+    return x / y
 
-# choose an option
-print('Select A / B / C / D :')
-option = input()
+# enter your option
+option = input("Choose a option: ")
+option = option.upper()
 
-# choose a right options
-if option == "A" or option == "a" or option == "b" or option == "B" or option == "c" or option == "C" or option == "D" or option == "d":
-    print("Let's Calculate.....")
-
-# for error 
-else:
-    print(" Sorry! You choose a wrong option.")
-    exit()
-
-# new instructions
-print("-----------------------------------")
-print("Always type large number at  first.")
-print("-----------------------------------")
-
-
-# enter first number
-print("Enter first number: ")
-num = input()
-num1 = int(num)
-
-# enter second number 
-print("Enter second number: ")
-num = input()
-num2 = int(num)
-
-# conditions of calculation 
-# for addition
-if option == "A" or option == "a":
-    print("Sum of two numbers is", num1 + num2)
-
-# for substraction
-elif option == "b" or option == "B":
-    print("Subtraction of two numbers is", num1 - num2)
-
-# for multiplication
-elif option == "c" or option == "C":
-    print("Multiplication of two numbers is", num1 * num2)
-
-# for division 
-else:
-    print("Division of two numbers is", num1 / num2)
-
+# Check if option is valid or not 
+def checkOption(x):
+    if option == "A" or option == "B" or option == "C" or option == "D":
+        
+        # collect input from user
+        firstNumber = int(input("Enter first number: "))
+        secondNumber = int(input("Enter second number: "))
+        
+        # calculation function
+        def calculation(x, y):
+           if option == "A":
+               print(f"Addition of {firstNumber} & {secondNumber} is {firstNumber + secondNumber}") # print addition of numbers
+           elif option == "B":
+               print(f"Subtraction of {firstNumber} & {secondNumber} is {firstNumber - secondNumber}") # print subtraction of numbers
+           elif option == "C":
+               print(f"Multiplication of {firstNumber} & {secondNumber} is {firstNumber * secondNumber}") # print multiplication of numbers
+           else:
+               print(f"Division of {firstNumber} & {secondNumber} is {firstNumber / secondNumber}") # print division of numbers
+       
+        calculation(firstNumber, secondNumber)
+    
+    # if option is invalid then print this
+    else:
+        print("Invalid Option")
+checkOption(option)
